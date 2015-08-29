006971d595694ae176b873347977005d380c93d5
dcubeauthor <dcube17site@gmail.com>
2015-08-30 02:09:01 +0530

    first commit

---
+++ app/assets/javascripts/demo.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#mail_table').dataTable();
+} );
---
+++ config/initializers/assets.rb
@@ -0,0 +1,20 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( creative.css )
+Rails.application.config.assets.precompile += %w( site_style.css )
+Rails.application.config.assets.precompile += %w( demo.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
